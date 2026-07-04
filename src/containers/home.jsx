564449e5b75341f6cb2a2b65539b86b378587add
Ajani King <ajaniking16@gmail.com>
2026-07-04 11:17:27 -0400

    completed homescreen

--- src/containers/home.jsx
+++ src/containers/home.jsx
@@ -17,8 +17,29 @@ const Home = () => {
         </div>
 
         {/* */}
-        <div className='relative z=10 flex flex-col items-center text-center pt-3 pointer-events-none'>
-          <h2 className='text-neutral-700 font-normal font-serif italic text-6xl md:text-7xl lg:text-8xl'>Welcome!</h2>
+        <div className='relative flex flex-col items-center justify-between text-center pointer-events-none h-full'>
+          <h2 className='text-neutral-700 font-normal font-serif italic text-5xl md:text-6xl lg:text-8xl tracking-widest'>Welcome!</h2>
+
+          <div className='w-full flex items-center justify-between px-12'>
+            <div className=' p-2 rounded-full bg-white shadow-sm flex items-center gap-2'>
+              <div className='w-6 h-6 bg-red-100 rounded-full flex items-center justify-center animate-pulse'>
+                <div className='w-3 h-3 bg-red-400 rounded-full'></div>
+              </div>
+
+              <span className='text-xs text-nuetral-700'>Tailoring spaces for the best living experience.</span>
+            </div>
+
+            {/* specialized */}
+            <p className="w-44 text-left text-xs tracking-wider text-neutral-700">
+              Specialized in Interior and Home Renovation
+              </p>
+          </div>
+
+          <div className='w-full flex items-center justify-between px-12 z-50'>
+            <h1 className='text-4xl md:text-6xl lg:text-8xl text-left font-bold font-sans text-neutral-800 tracking-widest uppercase'>NAJA <br />DESIGN</h1>
+
+            <h2 className='text-2xl md:text-4xl text-left font-bold text-neutral-800 tracking-wider uppercase'>Interior and Home <br />Designer</h2>
+          </div>
         </div>
     </div>
   )
